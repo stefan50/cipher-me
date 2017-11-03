4619af175f4f5fc6d887df577248952da717ebae
stefan50 <ste.angelov.tues19@gmail.com>
2017-11-03 08:03:20 -0400

    Through shell

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,18 @@
+require 'json'
+
 class MessagesController < ApplicationController
 	def new
 	end
 	def create
 		@message = Message.new(params.require(:messages).permit(:text))
-		@message.save
-		
+		@message.save		
 	end
 	def show
 		@message = Message.find(params[:id])
+	end
+	def api
+		json = File.read(params[:file])
+		obj = JSON.parse(json)
+		puts obj
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
 	get 'messages' => 'messages#new'
 	resources :messages
 	root 'messages#new'
+	post 'messages/api' => 'messages#api'
 end
