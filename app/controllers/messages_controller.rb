053061ffba2e4145d4fc8a04dc2dc4b383a6e7e1
stefan50 <ste.angelov.tues19@gmail.com>
2017-11-05 12:29:10 -0500

    Through shell

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -17,7 +17,7 @@ class MessagesController < ApplicationController
 	def api
 		json_data = params.permit(:message)
 		json_data = JSON.parse(json_data)
-		puts json_data
+		render plain: json_data
 		#@message = Message.new
 		#@message.text = json_data[:message].to_s
 		#@message.save
