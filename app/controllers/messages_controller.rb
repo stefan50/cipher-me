879b368d37f558613d3c33bdb802c0109fb9cf4a
stefan50 <ste.angelov.tues19@gmail.com>
2017-11-05 11:19:20 -0500

    Through shell

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -15,7 +15,7 @@ class MessagesController < ApplicationController
 	def api
 		json_data = params.permit(:message)
 		@obj = JSON.parse(json_data)
-		render plain: @obj
+		render json: @obj
 		#@mess = Message.find(@obj.message)
 		#render json: @mess
 	end
